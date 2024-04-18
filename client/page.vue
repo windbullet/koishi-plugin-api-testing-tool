9a593e566d279c9ba6436247f50e85dc75f35972
windbullet <2536810643@qq.com>
2024-04-18 12:32:31 +0800

    更新

--- client/page.vue
+++ client/page.vue
@@ -3,9 +3,14 @@
     <div class="text-center">
       <div class="join">
           <input v-model="url" class="input input-bordered w-100 join-item"/>
-          <select v-model="method" class="select select-bordered join-item">
+          <select v-model="method" class="select select-bordered join-item" @change="setContentType">
             <option>GET</option>
             <option>POST</option>
+            <option>DELETE</option>
+            <option>HEAD</option>
+            <option>OPTIONS</option>
+            <option>PUT</option>
+            <option>PATCH</option>
           </select>
           <button type="button" @click="http" class="btn join-item" :disabled="loading">
             <span :class="{'loading loading-spinner loading-md': loading}">{{ loading ? '' : "请求" }}</span>
@@ -20,6 +25,16 @@
         <a role="tab" class="tab" @click="tap('1')" v-bind:class="{'tab-active': active === '1'}">Query</a>
         <a role="tab" class="tab" @click="tap('2')" v-bind:class="{'tab-active': active === '2'}">Body</a>
         <a role="tab" class="tab" @click="tap('3')" v-bind:class="{'tab-active': active === '3'}">Headers</a>
+      </div>
+      <div v-if="['POST', 'PUT', 'PATCH'].includes(method)" @change="setContentType">
+        <div class="inline pr-3">
+          <input type="radio" value="application/x-www-form-urlencoded" v-model="contentType" name="radio-1" id="url" class="radio" checked />
+          <label for="url">urlencoded</label>
+        </div>
+        <div class="inline pr-3">
+          <input type="radio" value="application/json" v-model="contentType" name="radio-1" id="json" class="radio" />
+          <label for="json">json</label>
+        </div>
       </div>
       <div v-show="active === '1'">
         <div class="w-200 mx-auto h-full">
@@ -121,11 +136,11 @@
     <div v-if="resType === '1'" class="flex flex-col items-center">
       <label class="form-control">
         <div role="tablist" class="tabs tabs-lifted">
-          <input type="radio" name="my_tabs_2" role="tab" class="tab" aria-label="返回值" checked/>
+          <input type="radio" name="my_tabs_2" role="tab" class="tab" aria-label="返回值" :checked="def === '1'"/>
           <div role="tabpanel" class="tab-content bg-base-100 border-base-300 rounded-box p-6">
             <textarea v-model="result" rows="10" cols="55" readonly class="textarea textarea-bordered"></textarea>
           </div>
-          <input type="radio" name="my_tabs_2" role="tab" class="tab" aria-label="响应头" />
+          <input type="radio" name="my_tabs_2" role="tab" class="tab" aria-label="响应头" :checked="def === '2'"/>
           <div role="tabpanel" class="tab-content bg-base-100 border-base-300 rounded-box p-6">
             <textarea v-model="resHeaders" rows="10" cols="55" readonly class="textarea textarea-bordered"></textarea>
           </div>
@@ -155,27 +170,32 @@
       </label>
 
     </div>
-
+    <h1>{{ debug }}</h1>
   </k-layout>
 </template>
 
 <script setup lang="ts">
-  import { ref } from 'vue'
+  import { ref, watch } from 'vue'
+  import axios from 'axios'
+  let debug = ref("111")
   let url = ref('http://')
   let result = ref('')
   let method = ref('GET')
   let active = ref('1')
   let queryRows = ref([{name: '', value: ''}])
   let bodyRows = ref([{name: '', value: ''}])
-  let headersRows = ref([{name: 'content-type', value: 'application/x-www-form-urlencoded'}])
+  let headersRows = ref([{name: '', value: ''}])
   let urlWithQuery = ref()
   let resType = ref('1')
   let errorMessage = ref('')
   let loading = ref(false)
   let time = ref(0)
   let resHeaders = ref('')
+  let def = ref('1')
+  let contentType = ref('application/x-www-form-urlencoded')
   let controller
   let aborted = false
+
 
   function http() {
     loading.value = true
@@ -200,109 +220,63 @@
       }
     }
 
-    let config
-    controller = new AbortController()
-    if (method.value === 'GET') {
-      config = {
-        method: 'GET',
-        headers,
-        signal: controller.signal
-      }
-    } else if (method.value === 'POST') {
-      let type = headers['content-type']
-      if (type === 'application/json') {
-        let params = {}
-        for (let param of bodyRows.value) {
-          if (param.name && param.value) {
-            params[param.name] = param.value
-          }
-        }
-        config = {
-          method: 'POST',
-          headers,
-          body: JSON.stringify(params),
-          signal: controller.signal
-        }
-      } else if (type.startsWith('application/x-www-form-urlencoded')) {
-        let params = new URLSearchParams()
-        for (let param of bodyRows.value) {
-          if (param.name && param.value) {
-            params.append(param.name, param.value)
-          }
-        }
-        config = {
-          method: 'POST',
-          headers,
-          body: params,
-          signal: controller.signal
-        }
-      } else {
-        errorMessage.value = '请在headers内填写正确的content-type'
-        loading.value = false
-        return
-      }
-    }
-
     aborted = false
-
+    controller = new AbortController()
     let start = new Date().getMilliseconds()
 
-    fetch(urlWithQuery.value, config)
-      .then(res => {
-        if (!res.ok) {
-          errorMessage.value = res.statusText
+    let config = {
+      url: urlWithQuery.value, 
+      method: method.value, 
+      headers, 
+      signal: controller.signal, 
+      responseType: 'blob'
+    } as const
+
+    if (method.value === 'POST') {
+      let data = {}
+      for (let body of bodyRows.value) {
+        if (body.name && body.value) {
+          data[body.name] = body.value
+        }
+      }
+      config["data"] = data
+    }
+    
+
+    axios(config)
+      .then(async response => {
+        let end = new Date().getMilliseconds()
+        time.value = (end - start) / 1000
+        loading.value = false
+        resHeaders.value = JSON.stringify(response.headers, null, 2)
+        if (response.data.size === 0) {
+          resType.value = '1'
+          result.value = ''
+        } else if (response.data.type.startsWith('image')) {
+          resType.value = '2'
+          result.value = result.value = URL.createObjectURL(response.data)
+        } else if (response.data.type.startsWith('video')) {
+          resType.value = '3'
+          result.value = result.value = URL.createObjectURL(response.data)
+        } else if (response.data.type.startsWith('audio')) {
+          resType.value = '4'
+          result.value = result.value = URL.createObjectURL(response.data)
         } else {
-          errorMessage.value = ''
-          let resHeadersInside = ''
-          res.headers.forEach((value, name) => {
-            resHeadersInside += `${name}: ${value}\n`
-          })
-          resHeaders.value = resHeadersInside
-          let contentType = res.headers.get('content-type')
-          if (contentType.startsWith('application/json')) {
-            res.json()
-              .then(data => {
-                resType.value = '1'
-                result.value = JSON.stringify(data, null, "  ")
-              })
-          } else if (contentType.startsWith('image')) {
-            res.arrayBuffer()
-              .then(data => {
-                resType.value = '2'
-                result.value = `data:${contentType};base64,` + arrayBufferToBase64(data)
-              })
-          } else if (contentType.startsWith('video')) {
-            resType.value = '3'
-            res.blob()
-              .then(data => {
-                result.value = URL.createObjectURL(data)
-              })
-          } else if (contentType.startsWith('audio')) {
-            resType.value = '4'
-            res.blob()
-              .then(data => {
-                result.value = URL.createObjectURL(data)
-              })
+          if (response.data.type === 'application/json') {
+            resType.value = '1'
+            result.value = JSON.stringify(JSON.parse(await response.data.text()), null, 2)
           } else {
-            res.text()
-              .then(data => {
-                resType.value = '1'
-                result.value = data
-            })
+            resType.value = '1'
+            result.value = await response.data.text()
           }
-          loading.value = false
-          let end = new Date().getMilliseconds()
-          time.value = (end - start) / 1000
         }
       })
-      .catch(err => {
+      .catch(error => {
         loading.value = false
         if (!aborted) {
-          errorMessage.value = "请求失败，请按f12进入开发者工具查看详细错误信息\n" + err
+          errorMessage.value = "请求失败，请按f12进入开发者工具查看详细错误信息\n" + error
         }
-        
       })
-
   }
 
   function abort() {
@@ -323,13 +297,19 @@
   function addHeaderRow() {
     headersRows.value.push({name: '', value: ''})
   }
-  function arrayBufferToBase64( buffer ) {
-    var binary = '';
-    var bytes = new Uint8Array( buffer );
-    var len = bytes.byteLength;
-    for (var i = 0; i < len; i++) {
-        binary += String.fromCharCode( bytes[ i ] );
+  function setContentType() {
+    if (['POST', 'PUT', 'PATCH'].includes(method.value)) { 
+      let index = headersRows.value.findIndex(header => header.name.toLowerCase() === 'content-type')
+      if (index < 0) {
+        headersRows.value.unshift({name: 'content-type', value: contentType.value})
+      } else {
+        headersRows.value[index].value = contentType.value
+      }
+    } else {
+      let index = headersRows.value.findIndex(header => header.name.toLowerCase() === 'content-type')
+      if (index >= 0) {
+        headersRows.value.splice(index, 1)
+      }
     }
-    return window.btoa( binary );
-}
+  }
 </script>
